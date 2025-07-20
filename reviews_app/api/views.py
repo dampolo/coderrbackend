839bf92a6b045dd46b1438300d419a6d548dfabf
Damian Poloczek <damianpoloczek@gmail.com>
2025-07-20 20:55:50 +0200

    filterset_fields, ordering_fields, ordering

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -3,15 +3,21 @@ from reviews_app.models import Review
 from .serializer import ReviewSerializer
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 from reviews_app.permissions import IsReviewerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class CreateReviewView(ModelViewSet):
-    permission_classes = [IsReviewerOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['business_user', 'reviewer']
+    ordering_fields = ['updated_at', 'rating']
+    ordering = ['-updated_at']
 
 
 # serializer.is_valid() or serializer.save()
