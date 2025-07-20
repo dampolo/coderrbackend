c9eaec2912e6934da8797db50aedd47d0c2a0d3c
Damian Poloczek <damianpoloczek@gmail.com>
2025-07-20 17:16:23 +0200

    Review with validation

--- reviews_app/api/serializer.py
+++ reviews_app/api/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from reviews_app.models import Review
+from auth_app.models import Profile
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError as DjangoValidationError
 
 class ReviewSerializer(serializers.ModelSerializer):
     reviewer = serializers.SerializerMethodField(read_only=True)
@@ -25,13 +27,21 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def get_reviewer(self, obj):
         user = obj.reviewer
-        return {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-        }        
+        return user.id
+    
+    
+    def validate_rating(self, value):
+        if not (1 <= value <= 5):
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
 
     def create(self, validated_data):
         request = self.context.get("request")
+        user = request.user
+        business_user = validated_data.get("business_user")
+
+        if Review.objects.filter(reviewer=user, business_user=business_user).exists():
+            raise serializers.ValidationError("You have already reviewed this user.")
+            
         validated_data["reviewer"] = request.user
         return super().create(validated_data)
