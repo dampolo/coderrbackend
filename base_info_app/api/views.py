f35f6828809651a3db1518daec96f1efcda489e5
Damian Poloczek <damianpoloczek@gmail.com>
2025-07-21 22:02:45 +0200

    BaseInfoView remove code

--- base_info_app/api/views.py
+++ base_info_app/api/views.py
@@ -8,14 +8,9 @@ from offers_app.models import Offer
 
 class BaseInfoView(APIView):
       def get(self, request):
-        review_count = Review.objects.count()
-        average_rating = Review.objects.aggregate(Avg("rating", default=0))["rating__avg"]
-        business_profile_count = Profile.objects.filter(type="customer").count()
-        offer_count = Offer.objects.count()
-
         return Response({
-            "review_count": review_count,
-            "average_rating": average_rating,
-            "business_profile_count": business_profile_count,
-            "offer_count": offer_count
+            "review_count": Review.objects.count(),
+            "average_rating": Review.objects.aggregate(Avg("rating", default=0))["rating__avg"],
+            "business_profile_count": Profile.objects.filter(type="customer").count(),
+            "offer_count": Offer.objects.count()
         })
