ce15975dd40a23fa6421a872b2995b974546cf60
Damian Poloczek <damianpoloczek@gmail.com>
2025-07-30 23:35:57 +0200

    change of permisson in order

--- orders_app/permissions.py
+++ orders_app/permissions.py
@@ -1,11 +1,37 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsCustomerType(BasePermission):
+class IsOrderAccessAllowed(BasePermission):
     """
     Allows access only to users with type 'customer'.
     """
+
     def has_permission(self, request, view):
-        # Make sure the user is authenticated and has a 'type' attribute
-        return bool(request.user and request.user.is_authenticated and getattr(request.user, 'type', None) == 'customer')
+        user = request.user
+        user_type = getattr(user, 'type', None)
+
+        if not user.is_authenticated:
+            return False
+
+        if request.method == 'POST':
+            # Nur customer dürfen Bestellungen erstellen
+            return user_type == 'customer'
+
+        if request.method in ['PUT', 'PATCH']:
+            # Nur business user dürfen bearbeiten (Objektprüfung erfolgt später)
+            return user_type == 'business'
+
+        # Alle anderen Methoden wie GET, DELETE usw.
+        return True  # oder False, wenn du das einschränken willst
     
-    
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        # Nur business user darf seine eigene Bestellung bearbeiten
+        if request.method in ['PUT', 'PATCH']:
+            return (
+                getattr(request.user, 'type', None) == 'business' and 
+                obj.business_user == request.user
+            )
+
+        return True
