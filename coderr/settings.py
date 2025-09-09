77afc79768c6ad6a3d83816143ec7ad81ea14c47
Damian Poloczek <damianpoloczek@gmail.com>
2025-09-09 21:25:18 +0200

    new DB

--- coderr/settings.py
+++ coderr/settings.py
@@ -109,13 +109,21 @@ WSGI_APPLICATION = 'coderr.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
